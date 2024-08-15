5349b93debf005ac79032a88e4aa57916ca9dd02
Сорок два <sorokdva.developer@gmail.com>
2024-08-15 13:42:06 +0200

    :seedling: feat: Add initial categories data seeding
    Add seeding of initial categories into the 'categories' table. These cover
    various topics like lucid dreaming, dream interpretation, and creative
    dreams.

--- migrations/0000020-create-categories.js
+++ migrations/0000020-create-categories.js
@@ -36,6 +36,63 @@ module.exports = {
         allowNull: true,
       },
     });
+
+    await queryInterface.bulkInsert('categories', [
+      {
+        title: 'Rêve Lucide',
+        description: '- Techniques pour devenir lucide dans ses rêves\n- Conseils pour maintenir la lucidité',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Interprétation des Rêves',
+        description: '- Symbolisme des rêves\n- Signification des rêves récurrents\n- Méthode d\'interprétation des rêves',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Techniques de Souvenir des Rêves',
+        description: '- Journaux de rêves\n- Technique de rappel des rêves\n- Exercices pour améliorer la mémoire des rêves',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Rêves et Méditation',
+        description: '- La méditation dans la pratique du rêve lucide\n- Relaxation et préparation avant le sommeil\n- Visualisation et rêves',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Exploration des rêves',
+        description: '- Voyage onirique\n- Découverte de nouveaux mondes dans les rêves',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Rêves Thématiques',
+        description: '- Rêves d\'aventures\n- Rêves récurrents\n- Cauchemars et rêves d\'angoisse',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Rêves Créatifs',
+        description: '- Utilisation des rêvs pour la créativité artistique\n- Rêves pour l\'écriture ou l\'art\n- Inspirations oniriques',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Amélioration du sommeil',
+        description: '- Conseil pour un sommeil réparateur\n- Techniques de relaxation pour améliorer la qualité du sommeil\n- Rituel de coucher pour favoriser les rêves',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Rêves et Psychologie',
+        description: '- Analyse psychologique des rêves\n- Rêves et subconscient\n- Thérapies basées sur les rêves',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
   down: async (queryInterface) => {
     await queryInterface.dropTable('categories');
