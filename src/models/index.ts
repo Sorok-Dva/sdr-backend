be77d0cddc3fa5286082a490dd61b55a3122d28a
Сорок два <sorokdva.developer@gmail.com>
2024-08-15 10:59:41 +0200

    🛠️ feat: add comment and comment upvote models
    Include `Comment` and `CommentUpvote` models with associations.
    Enhance user, tutorial, and comment functionality with these additions.
    Consistency in naming conventions for associations is also updated.

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,25 +1,50 @@
 import { User } from './User'
 import { Event } from './Event'
+import { Comment } from './Comment'
 import { sequelize } from '../sequelize'
 import { Report } from './Report'
 import { Role } from './Role'
 import { Tutorial } from './Tutorial'
 import { UserDream } from './UserDream'
+import { CommentUpvote } from './CommentUpvote'
 
 // Associations
+
+// User and Role
 User.belongsTo(Role, { foreignKey: 'roleId', as: 'role' })
-Role.hasMany(User, { foreignKey: 'roleId', as: 'user' })
+Role.hasMany(User, { foreignKey: 'roleId', as: 'users' })
 
+// User and UserDream
 User.hasMany(UserDream, { foreignKey: 'userId', as: 'userDreams' })
-User.hasMany(Report, { foreignKey: 'userId', as: 'reports' })
-
 UserDream.belongsTo(User, { foreignKey: 'userId', as: 'user' })
-UserDream.hasMany(Report, { foreignKey: 'userDreamId', as: 'reports' })
 
+// User and Report
+User.hasMany(Report, { foreignKey: 'userId', as: 'reports' })
 Report.belongsTo(User, { foreignKey: 'userId', as: 'user' })
+
+// UserDream and Report
+UserDream.hasMany(Report, { foreignKey: 'userDreamId', as: 'reports' })
 Report.belongsTo(UserDream, { foreignKey: 'userDreamId', as: 'userDream' })
 
+// Event and User
 Event.belongsTo(User, { foreignKey: 'userId', as: 'user' })
+User.hasMany(Event, { foreignKey: 'userId', as: 'events' })
+
+// User and Comment
+User.hasMany(Comment, { foreignKey: 'userId', as: 'comments' })
+Comment.belongsTo(User, { foreignKey: 'userId', as: 'user' })
+
+// Tutorial and Comment
+Tutorial.hasMany(Comment, { foreignKey: 'tutorialId', as: 'comments' })
+Comment.belongsTo(Tutorial, { foreignKey: 'tutorialId', as: 'tutorial' })
+
+// Comment and CommentUpvote
+Comment.hasMany(CommentUpvote, { foreignKey: 'commentId', as: 'upvotes' })
+CommentUpvote.belongsTo(Comment, { foreignKey: 'commentId', as: 'comment' })
+
+// User and CommentUpvote
+User.hasMany(CommentUpvote, { foreignKey: 'userId', as: 'commentUpvotes' })
+CommentUpvote.belongsTo(User, { foreignKey: 'userId', as: 'user' })
 
 export {
   Role,
@@ -28,5 +53,7 @@ export {
   UserDream,
   Report,
   Tutorial,
+  Comment,
+  CommentUpvote,
   sequelize,
 }
