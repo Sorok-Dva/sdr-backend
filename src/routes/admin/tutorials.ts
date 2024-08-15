6621c74c6a2750af520c9af09147a4d348a31be3
Сорок два <sorokdva.developer@gmail.com>
2024-08-15 08:40:20 +0200

    :sparkles: feat: add update tutorial endpoint
    Add PATCH endpoint to update tutorial by ID. Include updating image, title,
    category, and content fields. Return updated tutorial or relevant error
    messages.

--- src/routes/admin/tutorials.ts
+++ src/routes/admin/tutorials.ts
@@ -29,4 +29,28 @@ router.post('/', async (req : Request, res : Response) => {
   }
 })
 
+router.patch('/:id', async (req : Request, res : Response) => {
+  const { id } = req.params
+  const { image, title, categoryId, content } = req.body
+
+  try {
+    const tutorial = await Tutorial.findByPk(id)
+
+    if (!tutorial) return res.status(404).send('Tutorial not found')
+
+    tutorial.image = image || tutorial.image
+    tutorial.categoryId = categoryId || tutorial.categoryId
+    tutorial.title = title || tutorial.title
+    tutorial.content = content || tutorial.content
+    tutorial.validated = false
+
+    await tutorial.save()
+
+    return res.status(201).json(tutorial)
+  } catch (error) {
+    console.error('Error while updating tutorial', error)
+    return res.status(500).json({ error: 'Error while updating tutorial' })
+  }
+})
+
 export { router as adminTutorialsRouter }
